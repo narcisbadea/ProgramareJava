a6a5c147bdd351786a80be3141bd5cd39d1c762a
NarcisBD <badeanarcis@gmail.ocm>
2021-10-27 23:10:52 +0300

    Lab2

---
+++ Lab2/src/problema2/Punct.java
@@ -0,0 +1,20 @@
+package problema2;
+
+public class Punct {
+	private double x, y;
+
+	public Punct(double x, double y) {
+		this.x = x;
+		this.y = y;
+
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+}
