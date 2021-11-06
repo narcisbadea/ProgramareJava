f79a83aee320674dbce05a2df0e1bae1248564eb
Facultate <badeanarcis@gmail.com>
2021-11-06 17:20:05 +0200

    Problem3
    age, birthdate, codJudet, formatCnp

--- Lab6/src/Problema3/MainApp.java
+++ Lab6/src/Problema3/MainApp.java
@@ -1,6 +1,13 @@
 package Problema3;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.Year;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Locale;
 import java.util.Scanner;
+import java.util.zip.ZipEntry;
 
 public class MainApp {
 
@@ -14,25 +21,47 @@ public class MainApp {
 			System.out.print("CNP: ");
 			cnp = input.nextLine();
 			try {
+
+				try {
+					Long.parseLong(cnp);
+				} catch (NumberFormatException e) {
+					throw new cnpExceptions("cifre");
+				}
+
 				if (cnp.length() != 13)
 					throw new cnpExceptions("size");
 
 				if (!"1256".contains(cnp.substring(0, 1)))
 					throw new cnpExceptions("cifraSexului");
-				
+
 				int ziNastere = Integer.parseInt(cnp.substring(3, 5));
-				if(ziNastere<1||ziNastere>31)
+				if (ziNastere < 1 || ziNastere > 31)
 					throw new cnpExceptions("ziuaNasterii");
-				
+
 				int lunaNastere = Integer.parseInt(cnp.substring(5, 7));
-				if(lunaNastere<1||lunaNastere>12)
+				if (lunaNastere < 1 || lunaNastere > 12)
 					throw new cnpExceptions("lunaNasterii");
-				
+
+				int codJudet = Integer.parseInt(cnp.substring(7, 9));
+				if (codJudet < 1 || codJudet > 42)
+					throw new cnpExceptions("codJudet");
 				valid = true;
 			} catch (cnpExceptions ex) {
 				System.out.println(ex.toString());
 			}
 		} while (!valid);
+		int anNastere = cnp.charAt(0) - '0';
+		anNastere = anNastere > 2 ? 2000 : 1900;
+		anNastere += Integer.parseInt(cnp.substring(1, 3));
+		LocalDate dataNasterii = LocalDate.of(anNastere, Integer.parseInt(cnp.substring(5, 7)),
+				Integer.parseInt(cnp.substring(3, 5)));
+		DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+	    String dataNasteriiFormatata = dataNasterii.format(myFormatObj);
+		System.out.println("Data nasterii: " + dataNasteriiFormatata);
+		int vartsa = Period.between(dataNasterii, LocalDate.now()).getYears();
+
+		System.out.println("Varsta: " + vartsa);
 	}
 
 }
--- Lab6/src/Problema3/cnpExceptions.java
+++ Lab6/src/Problema3/cnpExceptions.java
@@ -15,6 +15,8 @@ public class cnpExceptions extends Exception {
 
 	public String toString() {
 		switch (ex) {
+		case "cifre":
+			return "CNP-ul poate contine doar cifre!";
 		case "size":
 			return "CNP-ul trebuie sa aiba 13 cifre!";
 		case "cifraSexului":
@@ -23,7 +25,8 @@ public class cnpExceptions extends Exception {
 			return "Ziua nasterii nu este valida!";
 		case "lunaNasterii":
 			return "Luna nasterii nu este valida!";
-
+		case "codJudet":
+			return "Codul judetului nu este valid!";
 		default:
 			return null;
 		}
